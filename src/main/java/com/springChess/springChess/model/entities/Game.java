4dba1b8d37e1be65624d9b062d3bc051b9bac954
Miguel Salazar <miguelsalazar88@gmail.com>
2024-04-23 12:36:11 -0500

    can login and register

--- src/main/java/com/springChess/springChess/model/entities/Game.java
+++ src/main/java/com/springChess/springChess/model/entities/Game.java
@@ -3,7 +3,7 @@ package com.springChess.springChess.model.entities;
 import jakarta.persistence.*;
 
 @Entity
-public class git Game {
+public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
